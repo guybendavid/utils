bd94b9774b249f94ab9154a42c73798bb8ea9523
Guy <guybdd123@gmail.com>
2021-09-19 19:23:10 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const classNamesGenerator = (...items) => [...items].filter(string => string).join(' ');
+
+const timeDisplayer = (date) => {
+  let time = "";
+
+  const handleEdgeCases = (timePart) => {
+    return timePart = timePart < 10 ? `0${timePart}` : timePart;
+  };
+
+  if (date) {
+    let hours = new Date(date).getHours();
+    let minutes = new Date(date).getMinutes();
+    hours = handleEdgeCases(hours);
+    minutes = handleEdgeCases(minutes);
+    time = `${hours}:${minutes}`;
+  }
+
+  return time;
+};
+
+export { classNamesGenerator, timeDisplayer };
