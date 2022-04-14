1ec29b9aa752a54aa69d631f6895a3c459e6658a
Guy <guybdd123@gmail.com>
2022-04-14 18:52:05 +0300

    getErrors func

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const classNamesGenerator = (...items) => [...items].filter(item => item).join(' ');
 
+// To do: maybe remove it and use the new date functions in js instead
 const timeDisplayer = (date) => {
   let time = "";
 
@@ -18,4 +19,33 @@ const timeDisplayer = (date) => {
   return time;
 };
 
-module.exports = { classNamesGenerator, timeDisplayer };
+const getErrors = (payload) => {
+  const isOnlyOneField = (fields) => getInvalidFields(fields).length === 1;
+  const getInvalidFields = (fields) => fields.map((field, index) => `${index > 0 ? ` ${field}` : field}`);
+  const formatMessage = (message, fields) => `${message += isOnlyOneField(fields) ? ":" : "s:"} ${getInvalidFields(fields)}`;
+
+  let errors = "";
+  const emptyFields = [];
+  const sideWhiteSpacesFields = [];
+  const validString = /^[^\s]+(\s+[^\s]+)*$/;
+
+  Object.entries(payload).forEach(([key, value]) => {
+    if (!value || !validString.test(value)) {
+      !value ? emptyFields.push(key) : sideWhiteSpacesFields.push(key);
+    }
+  });
+
+  if (emptyFields.length > 0) {
+    errors = formatMessage("please send a non empty value for the field", emptyFields);
+  }
+
+  if (sideWhiteSpacesFields.length > 0) {
+    const message = formatMessage("please remove side white-spaces from the field", sideWhiteSpacesFields);
+    const isPreviousErrors = errors.length > 0;
+    errors += isPreviousErrors ? `<br /> ${message}` : message;
+  }
+
+  return errors;
+};
+
+module.exports = { classNamesGenerator, timeDisplayer, getErrors };
