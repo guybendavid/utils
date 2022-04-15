41c7e6366ae8bc34174d4ade9527fa892c2f3a84
Guy <guybdd123@gmail.com>
2022-04-15 14:03:27 +0300

    type update

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 declare type classNamesGeneratorItems = string | boolean | undefined;
 declare const classNamesGenerator: (...items: classNamesGeneratorItems[]) => string;
 declare const timeDisplayer: (date?: string | undefined) => string;
-declare const getFormValidationErrors: (payload: any) => string;
+declare const getFormValidationErrors: (payload: Record<string, unknown>) => string;
 export { classNamesGenerator, timeDisplayer, getFormValidationErrors };
