e2f9c768c9da8f9ca855ae74bccaaaa414db0884
Guy <bendavidguy1@gmail.com>
2026-01-19 03:14:17 +0200

    chore: simplify type definition for getFormattedTime function

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,6 @@ export declare type classNamesGeneratorItems = string | boolean | undefined;
 
 export declare const classNamesGenerator: (...items: classNamesGeneratorItems[]) => string;
 
-export declare const getFormattedTime: (date?: string | undefined) => string;
+export declare const getFormattedTime: (date?: string) => string;
 
 export declare const getFormValidationErrors: (payload: Record<string, unknown>) => { errors: string[]; message: string };
